d947df1bf514413448949307b293d1d8b70dfbe7
Aaryan Tripathi <ayt131102@gmail.com>
2024-08-15 20:53:47 +0530

    added some folders in set

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
-const express = require('express') //framework 
-const  colors = require('colors') //color in console
+import express from 'express';//framework 
+import  colors from 'colors'; //color in console
+import dotenv from 'dotenv'; //hide confidential data
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+//configure env
+dotenv.config();
+
 //rest object
 const app = express();
 
 //port
-const port = 8080;
- 
-//rest api 
+const port = process.env.port;
 
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+ 
+//database config
+connectDB();
+
+//rest api 
 app.get('/',(req,res) => {
     res.send({
         message:"Welcome to ecommerce app."
